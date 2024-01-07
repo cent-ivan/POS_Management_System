a9e48c84944eb47ff227ecdf69c48b6c26ede5b8
Vincent_Ivan <vpalomata25@gmail.com>
2024-01-07 20:44:50 +0800

    added searchbox (NO function yet)

--- Inventory.Designer.cs
+++ Inventory.Designer.cs
@@ -29,20 +29,53 @@
         private void InitializeComponent()
         {
             this.logoutButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // logoutButton
             // 
             this.logoutButton.BackColor = System.Drawing.Color.Red;
             this.logoutButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.logoutButton.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.logoutButton.Location = new System.Drawing.Point(1230, 29);
+            this.logoutButton.Font = new System.Drawing.Font("Segoe UI", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logoutButton.Location = new System.Drawing.Point(1275, 676);
             this.logoutButton.Name = "logoutButton";
-            this.logoutButton.Size = new System.Drawing.Size(94, 26);
+            this.logoutButton.Size = new System.Drawing.Size(71, 29);
             this.logoutButton.TabIndex = 1;
             this.logoutButton.Text = "Log Out";
             this.logoutButton.UseVisualStyleBackColor = false;
             this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.searchBox);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Location = new System.Drawing.Point(-4, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1452, 108);
+            this.panel1.TabIndex = 2;
+            // 
+            // searchBox
+            // 
+            this.searchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.searchBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBox.Location = new System.Drawing.Point(133, 40);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(431, 27);
+            this.searchBox.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(39, 40);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(88, 29);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Search";
             // 
             // Inventory
             // 
@@ -50,10 +83,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1446, 853);
             this.ControlBox = false;
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.logoutButton);
             this.Name = "Inventory";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Inventory";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -61,5 +97,8 @@
         #endregion
 
         private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Label label9;
     }
 }
