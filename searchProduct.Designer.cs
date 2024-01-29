959c4a051fc9e65983d70ec7cadbc884c64b0ccd
Vincent_Ivan <vpalomata25@gmail.com>
2024-01-29 21:35:44 +0800

    Added SearProduct Form complete function

---
+++ searchProduct.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace POS_Management_System
+{
+    partial class searchProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(searchProduct));
+            this.backButton = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.addCartButton = new System.Windows.Forms.Button();
+            this.productNum = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUnit = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDes = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUPC = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.invGrid = new System.Windows.Forms.DataGridView();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.backButton)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.productNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.backButton.Image = ((System.Drawing.Image)(resources.GetObject("backButton.Image")));
+            this.backButton.Location = new System.Drawing.Point(369, 10);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(28, 25);
+            this.backButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.backButton.TabIndex = 24;
+            this.backButton.TabStop = false;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label10.Location = new System.Drawing.Point(365, 39);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(42, 20);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Back";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.addCartButton);
+            this.panel2.Controls.Add(this.productNum);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.backButton);
+            this.panel2.Controls.Add(this.txtUnit);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.txtDiscount);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.txtDes);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.txtUPC);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtID);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel2.Location = new System.Drawing.Point(906, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(428, 788);
+            this.panel2.TabIndex = 26;
+            // 
+            // addCartButton
+            // 
+            this.addCartButton.BackColor = System.Drawing.SystemColors.Info;
+            this.addCartButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.addCartButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addCartButton.Location = new System.Drawing.Point(149, 614);
+            this.addCartButton.Name = "addCartButton";
+            this.addCartButton.Size = new System.Drawing.Size(144, 55);
+            this.addCartButton.TabIndex = 29;
+            this.addCartButton.Text = "Add To Cart";
+            this.addCartButton.UseVisualStyleBackColor = false;
+            this.addCartButton.Click += new System.EventHandler(this.addCartButton_Click);
+            // 
+            // productNum
+            // 
+            this.productNum.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productNum.Location = new System.Drawing.Point(195, 525);
+            this.productNum.Name = "productNum";
+            this.productNum.Size = new System.Drawing.Size(153, 31);
+            this.productNum.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 527);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 29);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Quantity:";
+            // 
+            // txtUnit
+            // 
+            this.txtUnit.BackColor = System.Drawing.SystemColors.Control;
+            this.txtUnit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUnit.Enabled = false;
+            this.txtUnit.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnit.Location = new System.Drawing.Point(195, 479);
+            this.txtUnit.Name = "txtUnit";
+            this.txtUnit.Size = new System.Drawing.Size(153, 30);
+            this.txtUnit.TabIndex = 27;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(35, 479);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(143, 29);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Unit Price:";
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.BackColor = System.Drawing.SystemColors.Control;
+            this.txtDiscount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDiscount.Enabled = false;
+            this.txtDiscount.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiscount.Location = new System.Drawing.Point(195, 428);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(153, 30);
+            this.txtDiscount.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(35, 425);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 29);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Discount:";
+            // 
+            // txtDes
+            // 
+            this.txtDes.BackColor = System.Drawing.SystemColors.Control;
+            this.txtDes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDes.Enabled = false;
+            this.txtDes.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDes.Location = new System.Drawing.Point(195, 222);
+            this.txtDes.Multiline = true;
+            this.txtDes.Name = "txtDes";
+            this.txtDes.Size = new System.Drawing.Size(202, 164);
+            this.txtDes.TabIndex = 23;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(35, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 29);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Description:";
+            // 
+            // txtUPC
+            // 
+            this.txtUPC.BackColor = System.Drawing.SystemColors.Control;
+            this.txtUPC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUPC.Enabled = false;
+            this.txtUPC.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUPC.Location = new System.Drawing.Point(195, 178);
+            this.txtUPC.Name = "txtUPC";
+            this.txtUPC.Size = new System.Drawing.Size(202, 30);
+            this.txtUPC.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 29);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "UPC:";
+            // 
+            // txtID
+            // 
+            this.txtID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtID.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(195, 139);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(70, 30);
+            this.txtID.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 29);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "ID:";
+            // 
+            // invGrid
+            // 
+            this.invGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.invGrid.Location = new System.Drawing.Point(12, 81);
+            this.invGrid.Name = "invGrid";
+            this.invGrid.RowHeadersVisible = false;
+            this.invGrid.RowHeadersWidth = 51;
+            this.invGrid.RowTemplate.Height = 24;
+            this.invGrid.Size = new System.Drawing.Size(876, 618);
+            this.invGrid.TabIndex = 27;
+            this.invGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.invGrid_CellClick);
+            // 
+            // searchBox
+            // 
+            this.searchBox.BackColor = System.Drawing.SystemColors.Control;
+            this.searchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.searchBox.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBox.Location = new System.Drawing.Point(163, 21);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(382, 30);
+            this.searchBox.TabIndex = 30;
+            this.searchBox.TextChanged += new System.EventHandler(this.searchBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 29);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Search";
+            // 
+            // searchProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1333, 723);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.invGrid);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "searchProduct";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "searchProduct";
+            this.Load += new System.EventHandler(this.searchProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.backButton)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.productNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox backButton;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUnit;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtDiscount;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUPC;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown productNum;
+        private System.Windows.Forms.DataGridView invGrid;
+        private System.Windows.Forms.Button addCartButton;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ searchProduct.cs
@@ -0,0 +1,166 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_Management_System
+{
+    public partial class searchProduct : Form
+    {
+        protected decimal unitPrice;
+        protected int discount;
+        protected decimal subtotal = 0;
+        private int prodQty;
+        public searchProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void searchProduct_Load(object sender, EventArgs e)
+        {
+            LoadTable();
+        }//On load events
+
+        private void invGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=pos_system_db");
+            using (conn)
+            {
+                conn.Open();
+                //checking the qunatity
+                string quantity_query = "SELECT Quantity FROM inventory_tbl;";
+                MySqlCommand cmd = new MySqlCommand(quantity_query, conn);
+                int stock = Convert.ToInt32(cmd.ExecuteScalar());
+
+                if (stock == 0)
+                {
+                    MessageBox.Show("Out of Stock");
+                }
+                else
+                {
+                    DataGridViewRow gridViewRow = invGrid.Rows[e.RowIndex];
+                    if (e.RowIndex != -1)
+                    {
+                        txtID.Text = gridViewRow.Cells[0].Value.ToString();
+                        txtUPC.Text = gridViewRow.Cells[1].Value.ToString();
+                        txtDes.Text = gridViewRow.Cells[2].Value.ToString();
+                        txtDiscount.Text = gridViewRow.Cells[3].Value.ToString();
+                        txtUnit.Text = gridViewRow.Cells[5].Value.ToString();
+                    }
+
+                    //Assigning table data to variables with discount computation
+                    discount = Convert.ToInt32(gridViewRow.Cells[3].Value.ToString());
+                    unitPrice = Convert.ToDecimal(gridViewRow.Cells[5].Value.ToString());
+
+                    //Auto assign the minimum and maximum in the number
+                    productNum.Minimum = 1;
+                    productNum.Maximum = Convert.ToInt32(gridViewRow.Cells[6].Value.ToString());
+                    prodQty = Convert.ToInt32(gridViewRow.Cells[6].Value.ToString());
+
+                }  
+            }
+            LoadTable();
+            
+        }//Click cell event
+
+        private void searchBox_TextChanged(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=pos_system_db");
+            using (conn)
+            {
+                conn.Open();
+                MySqlDataAdapter mysqlAdaptor = new MySqlDataAdapter($"SELECT invID AS ID, UPC, Description, Discount, Category, unitPrice AS Price, Quantity " +
+                $"FROM inventory_tbl WHERE CONCAT(UPC, Description) LIKE '%{searchBox.Text}%'", conn);
+                DataTable invTbl = new DataTable();
+                invTbl.Clear();
+                mysqlAdaptor.Fill(invTbl);
+
+                //inserting data from the table
+                invGrid.DataSource = invTbl;
+            }
+        }//Search Box code
+
+
+        private void addCartButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Add To Cart?", "Add To Cart", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (result == DialogResult.Yes)
+            {
+                //computing the discount
+                if (discount == 0)
+                {
+                    subtotal = unitPrice * Convert.ToInt32(productNum.Value);
+                }
+                else
+                {
+                    decimal discountTotal = unitPrice - (unitPrice * (discount / 100));
+                    subtotal = discountTotal * Convert.ToInt32(productNum.Value);
+                }
+                MySqlConnection conn = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=pos_system_db");
+                using (conn)
+                {
+                    conn.Open();
+                    string today = DateTime.Now.ToString("dd-MMM-yyyy, ddd hh:mm tt");
+                    string insertCart = "INSERT INTO cart_tbl(UPC, Description, unitPrice, Quantity, Discount, Subtotal, transDate) VALUES " +
+                        "(@upc, @description, @unitPrice, @quantity, @discount, @subtotal, @today);";
+                    MySqlCommand cartInsert = new MySqlCommand(insertCart, conn);
+                    cartInsert.Parameters.AddWithValue("@upc", txtUPC.Text);
+                    cartInsert.Parameters.AddWithValue("@description", txtDes.Text);
+                    cartInsert.Parameters.AddWithValue("@unitPrice", txtUnit.Text);
+                    cartInsert.Parameters.AddWithValue("@quantity", productNum.Value);
+                    cartInsert.Parameters.AddWithValue("@discount", txtDiscount.Text);
+                    cartInsert.Parameters.AddWithValue("@subtotal", subtotal);
+                    cartInsert.Parameters.AddWithValue("@today", today);
+                    int rowsInserted =  cartInsert.ExecuteNonQuery();
+
+                    int updatedQty = prodQty -  Convert.ToInt32(productNum.Value);
+                    string updateQuery = "UPDATE inventory_tbl SET Quantity = @updatedQty WHERE invID = @cartid";
+                    MySqlCommand updateInv = new MySqlCommand(updateQuery, conn);
+                    updateInv.Parameters.AddWithValue("@updatedQty", updatedQty);
+                    updateInv.Parameters.AddWithValue("@cartid", txtID.Text);
+                    rowsInserted = updateInv.ExecuteNonQuery();
+
+                    LoadTable();
+                }
+            }
+
+        }//Add to Cart Button
+
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var pos = new POS();
+            pos.ShowDialog();
+            Application.Exit();
+
+        }//Back Button
+
+
+        //--Mysql Methods-------------------------------------------------------------------------
+
+        private void LoadTable()
+        {
+            MySqlConnection conn = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=pos_system_db");
+            using (conn)
+            {
+                conn.Open();
+                string query = "SELECT invID AS ID, UPC, Description, Discount, Category, unitPrice AS Price, Quantity FROM inventory_tbl;";
+                MySqlDataAdapter mysqlAdapter = new MySqlDataAdapter(query, conn);
+                DataTable invtbl = new DataTable();
+                mysqlAdapter.Fill(invtbl);
+
+                //inserting data from the table
+                invGrid.DataSource = invtbl;
+            }
+
+        }//Loads or refreshes the table
+
+    }
+}
